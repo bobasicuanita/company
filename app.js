158cd1b2a93633d7d1e3e70a754a23489205d016
bobasicuanita <konstantinoskarasavvas@hotmail.com>
2019-02-20 21:59:00 +0200

    corrected file paths

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.use(session({
   secret: process.env.SECRET_KEY,
