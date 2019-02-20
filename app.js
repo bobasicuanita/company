679da5d58de53f02a97e27f754b75df1e01d4232
bobasicuanita <konstantinoskarasavvas@hotmail.com>
2019-02-20 21:49:20 +0200

    git ignore/Procfile

--- app.js
+++ app.js
@@ -776,6 +776,11 @@ app.post("/deleteFile", function(req, res) {
 
 //////////////////    Server listens to port 3000     ///////////////////////////
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if( port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server started successfully!");
 });
